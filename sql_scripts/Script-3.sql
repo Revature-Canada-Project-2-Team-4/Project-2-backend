19ceba1b04baf057cd58667ef013308453635893
Matt Northmore <rmnorthmore@gmail.com>
2021-01-07 11:24:00 -0500

    Adding initial db setup scripts

---
+++ sql_scripts/Script-3.sql
@@ -0,0 +1,59 @@
+-- Script for tradestars database schema
+
+drop schema if exists tradestars cascade;
+create schema tradestars;
+set schema 'tradestars';
+
+create table user_roles(
+	user_role_id serial unique not null primary key,
+	user_role varchar(10) not null
+);
+
+create table service_types(
+	service_type_id serial primary key,
+	service_type text not null
+);
+
+create table users(
+	user_id serial unique not null primary key,
+	username text unique not null,
+	"password" text not null,
+	first_name text not null,
+	last_name text not null,
+	email text unique not null,
+	role_id int not null references user_roles(user_role_id)
+);
+
+create table companies(
+	company_id serial primary key,
+	company_name text not null,
+	company_type text not null,
+	company_owner int not null references users(user_id)
+);
+
+create table services(
+	service_id serial primary key,
+	service_type int not null references service_types(service_type_id),
+	service_price numeric(10,2),
+	provided_by int not null references companies(company_id)
+);
+
+create table appointments (
+	appointment_id serial primary key,
+	customer_id int not null references users(user_id),
+	company_id int not null references companies(company_id),
+	appointment_start timestamp not null,
+	appointment_end timestamp not null,
+	appointment_confirmed boolean not null,
+	for_service int not null references services(service_id),
+	appointment_completed boolean not null
+);
+
+create table reviews (
+	review_id serial primary key,
+	review_text text,
+	reviewed_by int not null references users(user_id),
+	reviewed_for int not null references companies(company_id)
+);
+
+
