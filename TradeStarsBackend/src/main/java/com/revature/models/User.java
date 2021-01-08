7edb419fff5b56fa9267d635a597d55d31c554d7
Matt Northmore <rmnorthmore@gmail.com>
2021-01-08 14:22:25 -0500

    Added annotations to user model

--- TradeStarsBackend/src/main/java/com/revature/models/User.java
+++ TradeStarsBackend/src/main/java/com/revature/models/User.java
@@ -1,24 +1,44 @@
 package com.revature.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "users")
 public class User {
 	
+	@Id
+	@Column(name = "user_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int userId;
+	
+	@Column(name = "first_name")
 	private String firstName;
+	
+	@Column(name = "last_name")
 	private String lastName;
+	
+	@Column(name = "username")
 	private String username;
+	
+	@Column(name = "password")
 	private String password;
+	
+	@Column(name = "role_id")
 	private int userRole;
 	
-	
-	
-	
-	
+	@Column(name = "email")
+	private String email;
 	
 	public User() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	
 	public User(int userId, String firstName, String lastName, String username, String password, int userRole) {
 		super();
 		this.userId = userId;
@@ -29,7 +49,6 @@ public class User {
 		this.userRole = userRole;
 	}
 	
-	
 	public User(String firstName, String lastName, String username, String password, int userRole) {
 		super();
 		this.firstName = firstName;
@@ -39,55 +58,67 @@ public class User {
 		this.userRole = userRole;
 	}
 	
-	
-	
-	
-	
 	public int getUserId() {
 		return userId;
 	}
+	
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+	
 	public String getFirstName() {
 		return firstName;
 	}
+	
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	public String getLastName() {
 		return lastName;
 	}
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
 	public String getUsername() {
 		return username;
 	}
+	
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
 	public int getUserRole() {
 		return userRole;
 	}
+	
 	public void setUserRole(int userRole) {
 		this.userRole = userRole;
 	}
-	@Override
-	public String toString() {
-		return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", username="
-				+ username + ", password=" + password + ", userRole=" + userRole + "]";
+	
+	public String getEmail() {
+		return email;
 	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
 		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
 		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
 		result = prime * result + ((password == null) ? 0 : password.hashCode());
@@ -96,6 +127,7 @@ public class User {
 		result = prime * result + ((username == null) ? 0 : username.hashCode());
 		return result;
 	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -105,6 +137,11 @@ public class User {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
 		if (firstName == null) {
 			if (other.firstName != null)
 				return false;
@@ -132,9 +169,10 @@ public class User {
 		return true;
 	}
 	
-	
-	
-	
-	
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", username="
+				+ username + ", password=" + password + ", userRole=" + userRole + ", email=" + email + "]";
+	}
 
 }
