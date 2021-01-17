55674aa97aa9619975ff76ad0493a73f81ca7390
“your_github_username” <ramnindersingh@ramninders-MacBook-Pro.local>
2021-01-17 13:23:59 -0500

    create-service

--- TradeStarsBackend/src/main/java/com/revature/controllers/ServiceOfferingController.java
+++ TradeStarsBackend/src/main/java/com/revature/controllers/ServiceOfferingController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +38,13 @@ public class ServiceOfferingController {
 	public ResponseEntity<List<ServiceTypes>> findAllServiceTypes(){
 		
 		return new ResponseEntity<List<ServiceTypes>>(sos.getAllServiceTypes(), HttpStatus.OK);
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Services> saveNewUser(@RequestBody Services service){
+		//Here we call database to get data 
+		return new ResponseEntity<Services>(sos.saveServices(service), HttpStatus.CREATED);
 		
 	}
 }
