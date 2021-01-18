9637af3d0cbc7f8c3d683a32c3265080bf94c7c8
Matt Northmore <rmnorthmore@gmail.com>
2021-01-18 15:47:22 -0500

    Changing cors

--- TradeStarsBackend/src/main/java/com/revature/configuration/CorsConfig.java
+++ TradeStarsBackend/src/main/java/com/revature/configuration/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer{
 	public void addCorsMappings(CorsRegistry registry) {
 		// TODO Auto-generated method stub
 		registry.addMapping("/**") // all endpoints in the api
-        .allowedOrigins("http://localhost:1234", "http://localhost:3000", "http://ec2-54-175-141-33.compute-1.amazonaws.com:10000", "http://ec2-54-175-141-33.compute-1.amazonaws.com:8080") // YOU MIGHT HAVE TO ADD OR CHANGE
+        .allowedOrigins("http://localhost:1234", "http://localhost:3000", "http://ec2-54-175-141-33.compute-1.amazonaws.com") // YOU MIGHT HAVE TO ADD OR CHANGE
         .allowedMethods("PUT", "DELETE", "GET", "OPTIONS", "POST", "PATCH")
         .allowedHeaders("header1", "Content-Type", "Authorization")//YOU MIGHT HAVE TO ADD HEADERS
 //        .exposedHeaders("header1", "header2")
